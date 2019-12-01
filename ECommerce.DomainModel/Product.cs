9b152548f3d29e1b8dd627d790d14a8712ec702e
shahram1990 <shahram.afshar69@gmail.com>
2019-12-01 09:47:42 +0330

    Add Model Product

---
+++ ECommerce.DomainModel/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ECommerce.DomainModel
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Display(Name = "عنوان محصول")]
+        [Required(ErrorMessage = " فیلد{0} نمی تواند خالی باشد")]
+        [MaxLength(350, ErrorMessage = "فیلد{0} نمی تواند بیشتر {1} کاراکتر باشد")]
+        public string ProductTitle { get; set; }
+
+        [Display(Name = "توضیح مختصر")]
+        [Required(ErrorMessage = " فیلد{0} نمی تواند خالی باشد")]
+        [MaxLength(500, ErrorMessage = "فیلد{0} نمی تواند بیشتر {1} کاراکتر باشد")]
+        [DataType(DataType.MultilineText)]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "توضیح کامل")]
+        [Required(ErrorMessage = " فیلد{0} نمی تواند خالی باشد")]
+        [DataType(DataType.MultilineText)]
+        [AllowHtml]
+        public string Text { get; set; }
+
+        [Display(Name = "قیمت")]
+        [Required(ErrorMessage = " فیلد{0} نمی تواند خالی باشد")]
+        public int Price { get; set; }
+
+        [Display(Name = "تاریخ ثبت")]
+        [DisplayFormat(DataFormatString ="{0: yyyy/MM/dd}")]
+        public DateTime CreateDate { get; set; }
+
+        [Display(Name = "عکس")]
+        [MaxLength(50, ErrorMessage = "فیلد{0} نمی تواند بیشتر {1} کاراکتر باشد")]
+        public string ImageName { get; set; }
+
+        [Display(Name = "تعداد فروش")]
+        public Nullable<int> CountSale { get; set; }
+
+
+
+    }
+}
