d1a3af228b51e4426b5d61f34e5f3d3e19ed65b4
shahram1990 <shahram.afshar69@gmail.com>
2019-12-01 10:02:35 +0330

    Add Product_ProductGroup Model

---
+++ ECommerce.DomainModel/Product_ProductGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DomainModel
+{
+  public class Product_ProductGroup
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int ProductGroupId { get; set; }
+    }
+}
