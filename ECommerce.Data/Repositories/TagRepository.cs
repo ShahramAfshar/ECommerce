0784c49bc16c7f22d2c0e5bd56a7691f0a04d3d5
shahram1990 <shahram.afshar69@gmail.com>
2019-12-01 16:59:50 +0330

    Add TagRepository

---
+++ ECommerce.Data/Repositories/TagRepository.cs
@@ -0,0 +1,39 @@
+﻿using ECommerce.Data.DatabaseContext;
+using ECommerce.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repositories
+{
+     
+    public interface ITagRepository : IRepository<Tag>
+    {
+        //------Definition Private Functions Model -------------//
+        //   IList<User> GetActiveUsers();
+
+    }
+
+    public class TagRepository : Repository<Tag>, ITagRepository
+    {
+
+        private readonly DbContext db;
+        public TagRepository(DbContext dbContext) : base(dbContext)
+        {
+            this.db = (this.db ?? (MyDbContext)db);
+        }
+
+        //public IList<User> GetActiveUsers()
+        //{
+        //    var users = GetAll().Where(u => u.IsActive)
+        //        .ToList();
+        //    return users;
+        //}
+
+
+
+    }
+}
