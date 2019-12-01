3f2a2de524c660eed69460d1175a7c282a94c43b
shahram1990 <shahram.afshar69@gmail.com>
2019-12-01 10:25:00 +0330

    Add ProductRepository

---
+++ ECommerce.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using ECommerce.Data.DatabaseContext;
+using ECommerce.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repositories
+{
+
+
+    public interface IProductRepository : IRepository<Product>
+    {
+        //------Definition Private Functions Model -------------//
+        //   IList<User> GetActiveUsers();
+
+    }
+
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+
+        private readonly DbContext db;
+        public ProductRepository(DbContext dbContext) : base(dbContext)
+        {
+            this.db = (this.db ?? (MyDbContext)db);
+        }
+
+        //public IList<User> GetActiveUsers()
+        //{
+        //    var users = GetAll().Where(u => u.IsActive)
+        //        .ToList();
+        //    return users;
+        //}
+
+
+
+    }
+
+
+}
