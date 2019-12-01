cd2c0cdd41539d006dc0ec1cd9adf6466ef73372
shahram1990 <shahram.afshar69@gmail.com>
2019-12-01 09:57:46 +0330

    Add ProductGroup Model

---
+++ ECommerce.DomainModel/ProductGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DomainModel
+{
+    public class ProductGroup
+    {
+        [Key]
+        public int ProductGroupId { get; set; }
+
+        [Display(Name = "عنوان گروه")]
+        [Required(ErrorMessage = " فیلد{0} نمی تواند خالی باشد")]
+        public string Title { get; set; }
+
+
+        [Display(Name = "عنوان سر گروه")]
+        public Nullable<int> ParentId { get; set; }
+
+
+        //Relations
+        public virtual ICollection<ProductGroup>  ListProductGroup { get; set; }
+
+        [ForeignKey("ParentId")]
+        public virtual ProductGroup  OneProductGroup { get; set; }
+
+
+
+    }
+}
