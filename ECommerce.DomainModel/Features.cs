287ad9923898bbbe707214fd28261c79fad43ace
shahram1990 <shahram.afshar69@gmail.com>
2019-12-02 09:30:25 +0330

    Add Model Feature

---
+++ ECommerce.DomainModel/Features.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DomainModel
+{
+   public class Features
+    {
+        [Key]
+        public int FeatureID { get; set; }
+
+        [Display(Name = "ویژگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string FeatureTitle { get; set; }
+
+    //    public virtual ICollection<Product_Features> Product_Features { get; set; }
+    }
+}
