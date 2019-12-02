46aed618681171f9a178943b9157d8ae8ef747ad
shahram1990 <shahram.afshar69@gmail.com>
2019-12-02 11:02:04 +0330

    Add Model ProductGalery

---
+++ ECommerce.DomainModel/ProductGalery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DomainModel
+{
+  public  class ProductGalery
+    {
+        public int GalleryID { get; set; }
+
+        [Display(Name = "کالا")]
+        public int ProductID { get; set; }
+
+        [Display(Name = "تصویر")]
+        public string ImageName { get; set; }
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Title { get; set; }
+
+
+    }
+}
