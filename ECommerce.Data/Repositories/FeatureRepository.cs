f332b71e72d89b227fdf81ae267a03b8958d989e
shahram1990 <shahram.afshar69@gmail.com>
2019-12-02 09:43:27 +0330

    Add Repository Feature And Product_Feature

---
+++ ECommerce.Data/Repositories/FeatureRepository.cs
@@ -0,0 +1,40 @@
+﻿using ECommerce.Data.DatabaseContext;
+using ECommerce.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repositories
+{
+
+            public interface IFeatureRepository : IRepository<Feature>
+    {
+        //------Definition Private Functions Model -------------//
+        //   IList<User> GetActiveUsers();
+
+    }
+
+    public class FeatureRepository : Repository<Feature>, IFeatureRepository
+    {
+
+        private readonly DbContext db;
+        public FeatureRepository(DbContext dbContext) : base(dbContext)
+        {
+            this.db = (this.db ?? (MyDbContext)db);
+        }
+
+        //public IList<User> GetActiveUsers()
+        //{
+        //    var users = GetAll().Where(u => u.IsActive)
+        //        .ToList();
+        //    return users;
+        //}
+
+
+
+    }
+
+}
---
+++ ECommerce.Data/Repositories/Product_FeatureRepository.cs
@@ -0,0 +1,41 @@
+﻿using ECommerce.Data.DatabaseContext;
+using ECommerce.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repositories
+{
+
+
+    public interface IProduct_FeatureRepository : IRepository<Product_Feature>
+    {
+        //------Definition Private Functions Model -------------//
+        //   IList<User> GetActiveUsers();
+
+    }
+
+    public class Product_FeatureRepository : Repository<Product_Feature>, IProduct_FeatureRepository
+    {
+
+        private readonly DbContext db;
+        public Product_FeatureRepository(DbContext dbContext) : base(dbContext)
+        {
+            this.db = (this.db ?? (MyDbContext)db);
+        }
+
+        //public IList<User> GetActiveUsers()
+        //{
+        //    var users = GetAll().Where(u => u.IsActive)
+        //        .ToList();
+        //    return users;
+        //}
+
+
+
+    }
+
+}
