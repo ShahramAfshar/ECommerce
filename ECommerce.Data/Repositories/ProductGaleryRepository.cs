6e211c6df73b8099f079a5de30d4847dd450d417
shahram1990 <shahram.afshar69@gmail.com>
2019-12-02 11:06:08 +0330

    Add Repository ProductGalery

---
+++ ECommerce.Data/Repositories/ProductGaleryRepository.cs
@@ -0,0 +1,39 @@
+﻿using ECommerce.Data.DatabaseContext;
+using ECommerce.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repositories
+{
+     
+    public interface IProductGaleryRepository : IRepository<ProductGalery>
+    {
+        //------Definition Private Functions Model -------------//
+        //   IList<User> GetActiveUsers();
+
+    }
+
+    public class ProductGaleryRepository : Repository<ProductGalery>, IProductGaleryRepository
+    {
+
+        private readonly DbContext db;
+        public ProductGaleryRepository(DbContext dbContext) : base(dbContext)
+        {
+            this.db = (this.db ?? (MyDbContext)db);
+        }
+
+        //public IList<User> GetActiveUsers()
+        //{
+        //    var users = GetAll().Where(u => u.IsActive)
+        //        .ToList();
+        //    return users;
+        //}
+
+
+
+    }
+}
