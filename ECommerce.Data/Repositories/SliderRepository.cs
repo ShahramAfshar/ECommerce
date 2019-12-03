c5822fd286a3b94311de41a18d66b85581855ffe
shahram1990 <shahram.afshar69@gmail.com>
2019-12-03 09:58:15 +0330

    Add Repository Slider

---
+++ ECommerce.Data/Repositories/SliderRepository.cs
@@ -0,0 +1,39 @@
+﻿using ECommerce.Data.DatabaseContext;
+using ECommerce.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repositories
+{
+     
+    public interface ISliderRepository : IRepository<Slider>
+    {
+        //------Definition Private Functions Model -------------//
+        //   IList<User> GetActiveUsers();
+
+    }
+
+    public class SliderRepository : Repository<Slider>, ISliderRepository
+    {
+
+        private readonly DbContext db;
+        public SliderRepository(DbContext dbContext) : base(dbContext)
+        {
+            this.db = (this.db ?? (MyDbContext)db);
+        }
+
+        //public IList<User> GetActiveUsers()
+        //{
+        //    var users = GetAll().Where(u => u.IsActive)
+        //        .ToList();
+        //    return users;
+        //}
+
+
+
+    }
+}
