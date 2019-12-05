4a979ea3daa217f2fd4bfd790ca946be354c167e
shahram1990 <shahram.afshar69@gmail.com>
2019-12-05 10:06:05 +0330

    Add Order Detail Model

---
+++ ECommerce.DomainModel/Models/OrderDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DomainModel
+{
+  public  class OrderDetail
+    {
+        public int DetailID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+
+        //public virtual Orders Orders { get; set; }
+        //public virtual Products Products { get; set; }
+    }
+}
