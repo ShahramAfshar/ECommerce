3c0e7ce09fd40a28e7073deb6aa980130853b4a5
shahram1990 <shahram.afshar69@gmail.com>
2019-12-07 16:36:00 +0330

    Add Wish Controller And Update Views For Add To Wish

---
+++ ECommerce.Web/Controllers/WishController.cs
@@ -0,0 +1,53 @@
+﻿using ECommerce.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.Web.Controllers
+{
+    public class WishController : Controller
+    {
+        // GET: Wish
+        // GET: Shop
+        public int Get()
+        {
+            List<ShopCartItem> list = new List<ShopCartItem>();
+            // var sessions = HttpContext.Current.Session;
+            if (Session["Wish"] != null)
+            {
+                list = Session["Wish"] as List<ShopCartItem>;
+            }
+
+            return list.Sum(l => l.Count);
+        }
+
+        // GET: api/Shop/5
+        public int AddToWish(int id)
+        {
+            List<ShopCartItem> list = new List<ShopCartItem>();
+            //var sessions = HttpContext.Current.Session;
+            if (Session["Wish"] != null)
+            {
+                list = Session["Wish"] as List<ShopCartItem>;
+            }
+            if (list.Any(p => p.ProductID == id))
+            {
+                int index = list.FindIndex(p => p.ProductID == id);
+                list[index].Count += 1;
+            }
+            else
+            {
+                list.Add(new ShopCartItem()
+                {
+                    ProductID = id,
+                    Count = 1
+                });
+            }
+
+            Session["Wish"] = list;
+            return Get();
+        }
+    }
+}
