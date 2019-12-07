9a70222ffc52d6a83e9b345ff9c928eb6bf91ed8
shahram1990 <shahram.afshar69@gmail.com>
2019-12-07 16:19:26 +0330

    Update Shop Cart

--- ECommerce.Web/Controllers/ShopCartController.cs
+++ ECommerce.Web/Controllers/ShopCartController.cs
@@ -13,7 +13,48 @@ namespace ECommerce.Web.Controllers
     public class ShopCartController : Controller
     {
         private readonly UnitOfWork<MyDbContext> db = new UnitOfWork<MyDbContext>();
-        
+
+
+        // GET: Shop
+        public int Get()
+        {
+            List<ShopCartItem> list = new List<ShopCartItem>();
+            // var sessions = HttpContext.Current.Session;
+            if (Session["ShopCart"] != null)
+            {
+                list = Session["ShopCart"] as List<ShopCartItem>;
+            }
+
+            return list.Sum(l => l.Count);
+        }
+
+        // GET: api/Shop/5
+        public int AddToBasket(int id)
+        {
+            List<ShopCartItem> list = new List<ShopCartItem>();
+            //var sessions = HttpContext.Current.Session;
+            if (Session["ShopCart"] != null)
+            {
+                list = Session["ShopCart"] as List<ShopCartItem>;
+            }
+            if (list.Any(p => p.ProductID == id))
+            {
+                int index = list.FindIndex(p => p.ProductID == id);
+                list[index].Count += 1;
+            }
+            else
+            {
+                list.Add(new ShopCartItem()
+                {
+                    ProductID = id,
+                    Count = 1
+                });
+            }
+
+            Session["ShopCart"] = list;
+            return Get();
+        }
+
         public ActionResult ShowCart()
         {
             List<ShopCartItemViewModel> list = new List<ShopCartItemViewModel>();
@@ -37,7 +78,7 @@ namespace ECommerce.Web.Controllers
                 }
             }
 
-            return PartialView(list);
+            return View(list);
         }
 
         public ActionResult Index()
