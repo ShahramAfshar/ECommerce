9c7759605ad04ff70125c5faf4c9b846562c9443
shahram1990 <shahram.afshar69@gmail.com>
2019-12-08 11:28:00 +0330

    Add Comment Repository

---
+++ ECommerce.Data/Repositories/CommentRepository.cs
@@ -0,0 +1,41 @@
+﻿using ECommerce.Data.DatabaseContext;
+using ECommerce.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repositories
+{
+ 
+
+        public interface ICommentRepository : IRepository<Comment>
+        {
+            //------Definition Private Functions Model -------------//
+            //   IList<User> GetActiveUsers();
+
+        }
+
+        public class CommentRepository : Repository<Comment>, ICommentRepository
+    {
+
+            private readonly DbContext db;
+            public CommentRepository(DbContext dbContext) : base(dbContext)
+            {
+                this.db = (this.db ?? (MyDbContext)db);
+            }
+
+            //public IList<User> GetActiveUsers()
+            //{
+            //    var users = GetAll().Where(u => u.IsActive)
+            //        .ToList();
+            //    return users;
+            //}
+
+
+
+    
+    }
+}
