43eb4e78e7d0fe5208bef181ce24af0c8368f538
shahram1990 <shahram.afshar69@gmail.com>
2019-12-08 11:41:01 +0330

    Add DataAnnotations To Comment Model

--- ECommerce.DomainModel/Models/Comment.cs
+++ ECommerce.DomainModel/Models/Comment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,27 @@ namespace ECommerce.DomainModel
 {
    public class Comment
     {
+        [Key]
         public int CommentID { get; set; }
         public int ProductID { get; set; }
+
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
         public string Name { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
         public string Email { get; set; }
+
+        [Display(Name = "وب سایت")]
         public string WebSite { get; set; }
+
+        [Display(Name = "متن نظر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
         public string Message { get; set; }
+
+        [Display(Name = "تاریخ ثبت نظر")]
         public System.DateTime CreateDate { get; set; }
         public Nullable<int> ParentID { get; set; }
 
