cfa64b624033109db7bf58a16bb21a4c859d4be2
shahram1990 <shahram.afshar69@gmail.com>
2019-12-08 11:21:44 +0330

    Add Model Comment

---
+++ ECommerce.DomainModel/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DomainModel
+{
+   public class Comment
+    {
+        public int CommentID { get; set; }
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string WebSite { get; set; }
+        public string Message { get; set; }
+        public System.DateTime CreateDate { get; set; }
+        public Nullable<int> ParentID { get; set; }
+
+
+    }
+}
