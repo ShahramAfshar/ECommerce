8c7ba11e82019a4ea8eb3c3adc07e49c9cd1d656
shahram1990 <shahram.afshar69@gmail.com>
2019-12-18 16:14:29 +0330

    Add ContactUs Repository

---
+++ ECommerce.Data/Repositories/ContactUsRepository.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Data.DatabaseContext;
+using ECommerce.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repositories
+{
+     
+    public interface IContactUsRepository : IRepository<ContactUs>
+    {
+
+    }
+
+    public class ContactUsRepository : Repository<ContactUs>, IContactUsRepository
+    {
+
+        private readonly DbContext db;
+        public ContactUsRepository(DbContext dbContext) : base(dbContext)
+        {
+            this.db = (this.db ?? (MyDbContext)db);
+        }
+
+
+
+
+    }
+}
---
+++ ECommerce.DomainModel/Models/ContactUs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DomainModel
+{
+    public class ContactUs
+    {
+        [Key]
+        public int ContactUsId { get; set; }
+
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = " فیلد{0} نمی تواند خالی باشد")]
+        public string Name { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = " فیلد{0} نمی تواند خالی باشد")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+
+        [Display(Name = "پرسش")]
+        [Required(ErrorMessage = " فیلد{0} نمی تواند خالی باشد")]
+       [DataType(DataType.MultilineText)]
+        public int Question { get; set; }
+
+    }
+}
